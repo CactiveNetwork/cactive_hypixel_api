1dc076444bcf5b2ae3a3b8bacb584c5142c20f0e
cake <thecactivecake@gmail.com>
2022-08-12 22:13:55 +1000

    Add all endpoints

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ use serde::Deserialize;
 
 const API: &str = "https://hypixel.cactive.network/api/v3";
 
-/// Hello
 pub struct Client {
     key: String,
     cache: bool
@@ -15,6 +14,58 @@ pub struct NicknameHistory {
     pub active: bool,
     pub created_at: String,
     pub voided_at: String
+}
+
+#[derive(Deserialize)]
+pub struct PunishmentData {
+    pub id: String,
+    pub punishment_type: String,
+    pub uuid: String,
+    pub executor: Option<String>,
+    pub reason: String,
+    pub length: Option<u32>
+}
+
+#[derive(Deserialize)]
+pub struct PlayerDataNicknameHistory {
+    pub nickname: String,
+    pub active: Option<bool>,
+    pub created_at: String,
+    pub voided_at: Option<String>
+}
+
+#[derive(Deserialize)]
+pub struct PlayerDataInfractions {
+    pub id: String,
+    pub punishment_type: String,
+    pub executor: Option<String>,
+    pub reason: String,
+    pub length: Option<u32>
+}
+
+#[derive(Deserialize)]
+pub struct PlayerDataTracker {
+    pub server: Option<String>,
+    pub map: Option<String>,
+    pub proxy: Option<String>,
+    pub last_login: Option<String>
+}
+
+#[derive(Deserialize)]
+pub struct PlayerDataIPHistory {
+    pub ip: String,
+    pub login_at: String,
+    pub logout_at: Option<String>,
+    pub connection_proxy: Option<String>
+}
+
+#[derive(Deserialize)]
+pub struct PlayerData {
+    pub uuid: String,
+    pub nickname_history: Vec<PlayerDataNicknameHistory>,
+    pub infractions: Vec<PlayerDataInfractions>,
+    pub tracker: PlayerDataTracker,
+    pub ip_history: Option<Vec<PlayerDataIPHistory>>
 }
 
 #[derive(Deserialize)]
@@ -35,6 +86,14 @@ pub struct KeyData {
     pub endpoints: Vec<KeyEndpoints>
 }
 
+#[derive(Deserialize)]
+pub struct StaffTracker {
+    pub uuid: String,
+    pub rank: String,
+    pub online: Option<bool>
+}
+
+#[derive(Deserialize)]
 pub struct InternalError {
     pub r#type: String,
     pub code: u16,
@@ -58,10 +117,34 @@ pub struct APINicknameResponse {
 }
 
 #[derive(Deserialize)]
+pub struct APIPlayerData {
+    pub success: bool,
+    pub id: String,
+    pub data: Option<PlayerData>,
+    pub errors: Option<Vec<APIError>>
+}
+
+#[derive(Deserialize)]
 pub struct APIKeyData {
     pub success: bool,
     pub id: String,
     pub data: Option<KeyData>,
+    pub errors: Option<Vec<APIError>>
+}
+
+#[derive(Deserialize)]
+pub struct APIStaffTracker {
+    pub success: bool,
+    pub id: String,
+    pub data: Option<Vec<StaffTracker>>,
+    pub errors: Option<Vec<APIError>>
+}
+
+#[derive(Deserialize)]
+pub struct APIPunishmentData {
+    pub success: bool,
+    pub id: String,
+    pub data: Option<PunishmentData>,
     pub errors: Option<Vec<APIError>>
 }
 
@@ -102,6 +185,66 @@ impl Client {
             Err(err) => return Err(Client::map_internal_error(err.to_string()))
         };
         match request.json::<APINicknameResponse>().await {
+            Ok(json) => {
+                if json.success {
+                    Ok(json.data.unwrap())
+                } else {
+                    Err(Client::map_external_error(json.errors.unwrap()))
+                }
+            },
+            Err(err) => return Err(Client::map_internal_error(err.to_string()))
+        }
+    }
+
+    pub async fn player_data(&self, uuid: String) -> Result<PlayerData, Vec<InternalError>> {
+        let request = match reqwest::get(format!(
+            "{}/player-data?key={}&cache={}&uuid={}",
+            API.to_owned(), self.key, self.cache, uuid
+        )).await {
+            Ok(req) => req,
+            Err(err) => return Err(Client::map_internal_error(err.to_string()))
+        };
+        match request.json::<APIPlayerData>().await {
+            Ok(json) => {
+                if json.success {
+                    Ok(json.data.unwrap())
+                } else {
+                    Err(Client::map_external_error(json.errors.unwrap()))
+                }
+            },
+            Err(err) => return Err(Client::map_internal_error(err.to_string()))
+        }
+    }
+
+    pub async fn staff_tracker(&self) -> Result<Vec<StaffTracker>, Vec<InternalError>> {
+        let request = match reqwest::get(format!(
+            "{}/staff-tracker?key={}&cache={}",
+            API.to_owned(), self.key, self.cache
+        )).await {
+            Ok(req) => req,
+            Err(err) => return Err(Client::map_internal_error(err.to_string()))
+        };
+        match request.json::<APIStaffTracker>().await {
+            Ok(json) => {
+                if json.success {
+                    Ok(json.data.unwrap())
+                } else {
+                    Err(Client::map_external_error(json.errors.unwrap()))
+                }
+            },
+            Err(err) => return Err(Client::map_internal_error(err.to_string()))
+        }
+    }
+
+    pub async fn punishment_data(&self, id: String) -> Result<PunishmentData, Vec<InternalError>> {
+        let request = match reqwest::get(format!(
+            "{}/staff-tracker?key={}&cache={}&id={}",
+            API.to_owned(), self.key, self.cache, id
+        )).await {
+            Ok(req) => req,
+            Err(err) => return Err(Client::map_internal_error(err.to_string()))
+        };
+        match request.json::<APIPunishmentData>().await {
             Ok(json) => {
                 if json.success {
                     Ok(json.data.unwrap())
